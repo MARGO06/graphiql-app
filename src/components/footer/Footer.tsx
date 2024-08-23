0767679aa08480dc568c1f99992079d4970d569c
Margarita Masko <margaritam996@gmail.com>
2024-08-23 14:38:43 +0300

    feat:create footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import style from '@/components/footer/Footer.module.scss';
 
 export const Footer: React.FC = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={style.footer}>
+      <div className={style.footerContainer}>
+        <Link href="https://github.com/MARGO06" className={style.gitHub} target="_blank">
+          GITHUB
+        </Link>
+        <p className={style.year}>2024</p>
+        <Link href="https://rs.school/react/" className={style.school} target="_blank">
+          <Image
+            src={'/rss-logo.svg'}
+            alt="flag"
+            className={style.schoolLogo}
+            width={32}
+            height={32}
+            priority
+          />
+        </Link>
+      </div>
+    </footer>
+  );
 };
