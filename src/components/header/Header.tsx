8581ef7560157141829adbdbfd6fc1144fec848e
Vadzim <49336945+comtvset@users.noreply.github.com>
2024-08-27 10:48:40 +0200

    feat: remove default favicon, add favicon, correct style, add burger-menu
    * feat: remove default favicon, add favicon, correct style, add burger-menu
    
    * fix: correct PR template

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,22 +1,43 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import style from '@/components/header/Header.module.scss';
 import Link from 'next/link';
 import { Languages } from '@/components/language/Languages';
 import { Registration } from '@/components/registration/Registration';
 import { usePathname } from 'next/navigation';
+import Image from 'next/image';
 
 export const Header: React.FC = () => {
   const pathname = usePathname();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className={style.header}>
       <nav className={style.navigation}>
         <Link href={'/'} className={`${style.logo} ${pathname === '/' ? style.active : ''}`}>
-          APIQuest
+          <Image
+            src="/APIQuest-logo.png"
+            alt="logo"
+            width={60}
+            height={60}
+            quality={100}
+            priority
+          />
         </Link>
-        <Registration />
+        <div className={style.burger} onClick={toggleMenu}>
+          <span className={isMenuOpen ? style.burgerOpen : ''}></span>
+          <span className={isMenuOpen ? style.burgerOpen : ''}></span>
+          <span className={isMenuOpen ? style.burgerOpen : ''}></span>
+        </div>
+        <div className={`${style.menu} ${isMenuOpen ? style.menuOpen : ''}`}>
+          <Registration />
+          <Languages />
+        </div>
       </nav>
-      <Languages />
     </header>
   );
 };
