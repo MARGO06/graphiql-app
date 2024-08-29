764e67a2ab2c8ccb221de4be6f26cfc216e5bb58
maria.akulova <maria.akulova.dragon@gmail.com>
2024-08-29 22:12:19 +0200

    RG-33 add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+'use client';
+import { useTranslations } from 'next-intl';
+import { usePathname, useRouter } from 'next/navigation';
+
+export default function NotFound() {
+  const pathname = usePathname();
+  const router = useRouter();
+  const t = useTranslations('NotFound');
+
+  return (
+    <aside>
+      <h2>
+        {t('welcome')} {pathname}
+      </h2>
+      <p>{t('info')}</p>
+      <button onClick={() => router.push('/main')}> {t('back')}</button>
+    </aside>
+  );
+}
