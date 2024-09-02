4f6b723ef85bc919fab3546b02d0a0463ea5cdc9
comtvset <comtvset@gmail.com>
2024-09-02 02:54:07 +0200

    feat: restfull client template

---
+++ src/components/request/RequestWindow.tsx
@@ -0,0 +1,20 @@
+import style from './RequestWindow.module.scss';
+
+export const RequestWindow = () => {
+  return (
+    <>
+      <div className={style.selectContainer}>
+        <label htmlFor="contentType"></label>
+        <select id="contentType">
+          <option value="application/json">JSON (application/json)</option>
+          <option value="text/plain">TEXT (text/plain)</option>
+        </select>
+      </div>
+
+      <div className={style.editorContainer}>
+        <label htmlFor="requestBody"></label>
+        <textarea rows={10} />
+      </div>
+    </>
+  );
+};
