5a526e31a7fc5abed441e4fb017e687bdd73e3f2
maria.akulova <maria.akulova.dragon@gmail.com>
2024-09-04 20:19:40 +0200

    add header test(without registration form)

---
+++ src/components/header/Header.test.tsx
@@ -0,0 +1,56 @@
+import '@testing-library/jest-dom';
+import { screen, fireEvent } from '@testing-library/react';
+import { Header } from '@/components/header/Header';
+import { useRouter } from 'next/navigation';
+import { customRender } from '@/utils/CustomeRenderTest';
+
+describe('Header Component', () => {
+  it('renders the header with the logo and switch language element', () => {
+    customRender(<Header />);
+
+    const logo = screen.getByRole('link', { name: 'site-logo' });
+    expect(logo).toBeInTheDocument();
+    expect(logo).toHaveAttribute('href', '/');
+
+    const switchLanguage = screen.getByRole('combobox');
+    expect(switchLanguage).toBeInTheDocument();
+  });
+
+  it('sign-out is presented for logged user and user can click the sign-out button', () => {
+    customRender(<Header />);
+
+    const signOutButton = screen.getByRole('button');
+    expect(signOutButton).toBeInTheDocument();
+    fireEvent.click(signOutButton);
+    expect(useRouter().replace).toHaveBeenCalledTimes(1);
+    expect(useRouter().replace).toHaveBeenCalledWith('/');
+  });
+
+  it('toggles the menu on burger icon click', () => {
+    window.innerWidth = 500;
+    window.dispatchEvent(new Event('resize'));
+    customRender(<Header />);
+
+    const burgerButton = screen.getByTestId('burger-icon');
+    fireEvent.click(burgerButton);
+    expect(screen.getByTestId('burger-menu').closest('div')).toHaveClass('menuOpen');
+
+    const burgerClose = screen.getByTestId('burger-close');
+    fireEvent.click(burgerClose);
+    expect(screen.getByTestId('burger-menu').closest('div')).not.toHaveClass('menuOpen');
+  });
+
+  it('user is not logged and can clicks on sign in button', () => {
+    customRender(<Header />, { token: null });
+
+    const signInButton = screen.getByTestId('sign-in');
+    expect(signInButton).toBeInTheDocument();
+  });
+
+  it('user is not logged and can clicks on sign up button', () => {
+    customRender(<Header />, { token: null });
+
+    const signUpButton = screen.getByTestId('sign-up');
+    expect(signUpButton).toBeInTheDocument();
+  });
+});
