36d6beb52981c2cf929eabbbb6bc5f295d730099
comtvset <comtvset@gmail.com>
2024-09-09 18:33:29 +0200

    fix: rename key(from currentUrl to baseUrl) on LS

--- src/services/baseURL.ts
+++ src/services/baseURL.ts
@@ -2,18 +2,18 @@
 
 export function saveCurrentUrlToLocalStorage() {
   if (typeof window !== 'undefined') {
-    let currentUrl = window.location.href;
-    if (currentUrl.endsWith('/')) {
-      currentUrl = currentUrl.slice(0, -1);
+    let baseUrl = window.location.href;
+    if (baseUrl.endsWith('/')) {
+      baseUrl = baseUrl.slice(0, -1);
     }
 
-    localStorage.setItem('currentUrl', currentUrl);
+    localStorage.setItem('baseUrl', baseUrl);
   }
 }
 
 export function getCurrentUrlFromLocalStorage() {
   if (typeof window !== 'undefined') {
-    const savedUrl = localStorage.getItem('currentUrl');
+    const savedUrl = localStorage.getItem('baseUrl');
     return savedUrl;
   }
   return null;
