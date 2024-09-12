5eb3b6b17a96f092dbe724f904cdff015fa26dd6
comtvset <comtvset@gmail.com>
2024-09-12 13:48:27 +0200

    feat: add time to history(localStorage)

--- src/services/saveToHistory.ts
+++ src/services/saveToHistory.ts
@@ -2,17 +2,26 @@ export interface HistoryItem {
   method: string;
   url: string;
   urlBase64: string;
+  date: string;
+  headerParams: string;
 }
 
-export const saveToHistory = (method: string, url: string, urlBase64: string) => {
+export const saveToHistory = (
+  method: string,
+  url: string,
+  urlBase64: string,
+  headerParams: string,
+) => {
   if (typeof window !== 'undefined') {
     const existingHistory = localStorage.getItem('history');
     const historyArray: HistoryItem[] = existingHistory ? JSON.parse(existingHistory) : [];
 
     const newEntry: HistoryItem = {
+      date: new Date().toISOString(),
       method,
       url,
       urlBase64,
+      headerParams,
     };
 
     historyArray.push(newEntry);
