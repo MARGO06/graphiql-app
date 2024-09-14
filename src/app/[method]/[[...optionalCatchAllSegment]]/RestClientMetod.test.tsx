db0ece3b3c53439aa24e2d62192758e06193c969
Maria <99416741+maria-akulova@users.noreply.github.com>
2024-09-14 21:46:03 +0200

    RG-90_FixTestRaiseTo80 (#56)
    * fix: skipped tests
    
    * add test to utils
    
    * add tests for url operations from utils
    
    * add test for Header on RestClient

--- src/app/[method]/[[...optionalCatchAllSegment]]/RestClientMetod.test.tsx
+++ src/app/[method]/[[...optionalCatchAllSegment]]/RestClientMetod.test.tsx
@@ -1,10 +1,17 @@
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import RESTfull from './page';
 import { useParams } from 'next/navigation';
 import { render } from '@/utils/CustomeRenderTest';
 
+jest.mock('next/navigation', () => ({
+  useSearchParams: jest.fn(() => []),
+  useRouter: jest.fn(),
+  usePathname: jest.fn(),
+  useParams: jest.fn(),
+}));
+
 describe('RESTfull Factory', () => {
-  it.skip('renders RestClient with valid method and url', () => {
+  it('renders RestClient with valid method and url', () => {
     (useParams as jest.Mock).mockReturnValue({
       method: 'GET',
       optionalCatchAllSegment: ['/aHR0cHM6Ly9wb2tlYXBpLmNvL2FwaS92Mg=='],
@@ -16,7 +23,7 @@ describe('RESTfull Factory', () => {
     expect(header).toBeInTheDocument();
   });
 
-  it.skip('renders NotFound with invalid method', () => {
+  it('renders NotFound with invalid method', () => {
     (useParams as jest.Mock).mockReturnValue({
       method: 'INVALID_METHOD',
       optionalCatchAllSegment: ['aHR0cHM6Ly9leGFtcGxlLmNvbQ=='],
@@ -28,7 +35,7 @@ describe('RESTfull Factory', () => {
     expect(header).toBeInTheDocument();
   });
 
-  it.skip('correct handles empty optionalCatchAllSegment', () => {
+  it('correct handles empty optionalCatchAllSegment', () => {
     (useParams as jest.Mock).mockReturnValue({
       method: 'GET',
       optionalCatchAllSegment: '',
@@ -38,5 +45,39 @@ describe('RESTfull Factory', () => {
 
     const header = screen.getByText('RESTfull client');
     expect(header).toBeInTheDocument();
+  });
+
+  it('renders RestClient with valid method and url', () => {
+    (useParams as jest.Mock).mockReturnValue({
+      method: 'GET',
+      optionalCatchAllSegment: ['/aHR0cHM6Ly9wb2tlYXBpLmNvL2FwaS92Mg=='],
+    });
+
+    render(<RESTfull />);
+
+    const addHeaderButton = screen.getByRole('button', { name: 'Add Header' });
+
+    fireEvent.click(addHeaderButton);
+
+    const keyHeader = screen.getByPlaceholderText('key (header)');
+    const keyValue = screen.getByPlaceholderText(/value/i);
+    const removeKeyButton = screen.getByTestId('remove-key');
+
+    expect(keyHeader).toBeInTheDocument();
+    expect(keyValue).toBeInTheDocument();
+
+    fireEvent.click(removeKeyButton);
+
+    expect(keyHeader).not.toBeInTheDocument();
+
+    fireEvent.click(screen.getByRole('button', { name: 'Add Header' }));
+    const addKeyButton = screen.getByTestId('add-key');
+    fireEvent.click(addKeyButton);
+    fireEvent.change(screen.getAllByPlaceholderText('key (header)')[0], {
+      target: { value: 'content' },
+    });
+    fireEvent.change(screen.getAllByPlaceholderText(/value/i)[0], {
+      target: { value: 'json' },
+    });
   });
 });
--- src/utils/ConvertStringsDates.ts
+++ src/utils/ConvertStringsDates.ts
@@ -1,6 +1,6 @@
 export function dateToUnixEpoch(creationDate: Date): number {
   const date = creationDate instanceof Date ? creationDate : new Date(creationDate);
-
+  /* c8 ignore next 3 */
   if (isNaN(date.getTime())) {
     throw new Error('Invalid date');
   }
