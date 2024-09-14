7785e1742012e485a7c3c0e826d387a5384ca7c9
maria.akulova <maria.akulova.dragon@gmail.com>
2024-09-14 21:24:01 +0200

    add test for Header on RestClient

--- src/app/[method]/[[...optionalCatchAllSegment]]/RestClientMetod.test.tsx
+++ src/app/[method]/[[...optionalCatchAllSegment]]/RestClientMetod.test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import RESTfull from './page';
 import { useParams } from 'next/navigation';
 import { render } from '@/utils/CustomeRenderTest';
@@ -45,5 +45,39 @@ describe('RESTfull Factory', () => {
 
     const header = screen.getByText('RESTfull client');
     expect(header).toBeInTheDocument();
+  });
+
+  it('renders RestClient with valid method and url', () => {
+    (useParams as jest.Mock).mockReturnValue({
+      method: 'GET',
+      optionalCatchAllSegment: ['/aHR0cHM6Ly9wb2tlYXBpLmNvL2FwaS92Mg=='],
+    });
+
+    render(<RESTfull />);
+
+    const addHeaderButton = screen.getByRole('button', { name: 'Add Header' });
+
+    fireEvent.click(addHeaderButton);
+
+    const keyHeader = screen.getByPlaceholderText('key (header)');
+    const keyValue = screen.getByPlaceholderText(/value/i);
+    const removeKeyButton = screen.getByTestId('remove-key');
+
+    expect(keyHeader).toBeInTheDocument();
+    expect(keyValue).toBeInTheDocument();
+
+    fireEvent.click(removeKeyButton);
+
+    expect(keyHeader).not.toBeInTheDocument();
+
+    fireEvent.click(screen.getByRole('button', { name: 'Add Header' }));
+    const addKeyButton = screen.getByTestId('add-key');
+    fireEvent.click(addKeyButton);
+    fireEvent.change(screen.getAllByPlaceholderText('key (header)')[0], {
+      target: { value: 'content' },
+    });
+    fireEvent.change(screen.getAllByPlaceholderText(/value/i)[0], {
+      target: { value: 'json' },
+    });
   });
 });
